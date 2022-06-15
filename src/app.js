5ca23c50f11ae75ea124c39deae02c8d1e7be38d
Abdullah Waseem <abdullah.waseem@ontariotechu.net>
2022-06-15 18:57:44 -0400

    create database models and import into auth and main

--- src/app.js
+++ src/app.js
@@ -7,131 +7,13 @@ const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const authenticate = require('./authenticate').authenticate;
 const generateToken = require('./authenticate').generateToken;
+const User = require('./models/user.js');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 const saltRounds = Number(process.env.SALT_ROUNDS);
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-
-const postSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    content: {
-        data: Buffer,
-        contentType: String
-    },
-    caption: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }
-});
-
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        validator: (email) => {
-            if (!validator.isEmail(email)) {
-                throw new Error('Invalid email');
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 8,
-        trim: true,
-    },
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    age: {
-        type: Number,
-        required: true,
-        min: 0,
-    },
-    avatar: {
-        data: Buffer,
-        contentType: String
-    },
-    profileStatus: {
-        type: String,
-        required: true,
-        trim: true,
-        default: 'private'
-    },
-    requests: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-    }],
-    followers: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-    }],
-    following: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-    }],
-    posts: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Post',
-    }],
-    token: {
-        type: String,
-        trim: true,
-        unique: true,
-        required: true,
-        validate: {
-            validator: (token) => {
-                if (!validator.isJWT(token)) {
-                    throw new Error('Invalid token');
-                }
-            }
-        }
-
-    },
-    refreshToken: {
-        type: String,
-        trim: true,
-        unique: true,
-        required: true,
-        validate: {
-            validator: (token) => {
-                if (!validator.isJWT(token)) {
-                    throw new Error('Invalid token');
-                }
-            }
-        }
-    }
-
-});
-
-const Post = mongoose.model('Post', postSchema);
-const User = mongoose.model('User', userSchema);
 
 
 app.post('/register', (req, res) => {
@@ -142,7 +24,6 @@ app.post('/register', (req, res) => {
         if (!err) {
             const password = await hash;
             const user = new User({ username, email, password, name, age, avatar, profileStatus, token, refreshToken });
-            console.log(user);
             user.save((error) => {
                 if (!error) {
                     res.status(200).json({
