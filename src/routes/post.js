32763414ae32f81c55a2561d27985a65bfa6c6c2
Abdullah Waseem <abdullah.waseem@ontariotechu.net>
2022-06-29 18:32:30 -0400

    update post route

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,27 +1,56 @@
 const express = require('express');
 const router = express.Router();
 const validator = require('validator');
+const fs = require('fs');
 const authenticate = require('../authentication/authenticate.js').authenticate;
 const User = require('../models/user.js');
 const Post = require('../models/post.js');
 
-router.post('/post', authenticate, (req, res) => {
+router.post('/post', authenticate, async (req, res) => {
     const username = req.body.user.username;
     const post = req.body.post;
-    User.findOneAndUpdate({ username: username }, { $push: { posts: post } }, { new: true }, (error, user) => {
-        if (!error) {
-            res.status(200).json({
-                message: 'Post created successfully',
-                post: post
+    const image = await fs.readFileSync(post.content);
+    User.findOne({ username: username }, (err, user) => {
+        if (!err) {
+            //define new post as a constant and save it seprately
+            new Post({ title: post.title, content: image, caption: post.caption, createdAt: Date.now(), owner: user._id }, (err, post) => {
+               post.save((err, post) => {
+                if (!err) {
+                    user.posts.push(post._id);
+                    user.save((err) => {
+                        if (!err) {
+                            res.status(201).json({
+                                message: 'Post created',
+                                post: post
+                            });
+                        } else {
+                            res.status(503).json({
+                                message: 'Error saving post',
+                                error: err
+                            });
+
+                        }
+                    });
+                }
+                else {
+                    res.status(503).json({
+                        message: 'Error saving post',
+                        error: err
+                    });
+                }
             });
+
+            });
+
         } else {
-            res.status(400).json({
-                message: 'Error creating post',
-                error: error
+            res.status(500).json({
+                message: 'Error finding user',
+                error: err
             });
         }
-    }
-    );
+
+    });
+
 });
 
 router.delete('/post', authenticate, (req, res) => {
@@ -31,7 +60,7 @@ router.delete('/post', authenticate, (req, res) => {
         res.status(400).json({
             message: 'Invalid post id'
         });
-    } else{
+    } else {
         User.findOneAndUpdate({ username: username }, { $pull: { posts: { _id: postId } } }, { new: true }, (error, user) => {
             if (!error) {
                 res.status(200).json({
