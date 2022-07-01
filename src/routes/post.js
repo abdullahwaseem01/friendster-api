23026cf9f31b73051062dce6ea2c8bc57d7e59f1
Abdullah Waseem <abdullah.waseem@ontariotechu.net>
2022-07-01 18:00:09 -0400

    content type validation

--- src/routes/post.js
+++ src/routes/post.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const validator = require('validator');
 const path = require('path');
 const fs = require('fs');
+const _ = require('lodash');
 const authenticate = require('../authentication/authenticate.js').authenticate;
 const User = require('../models/user.js');
 const Post = require('../models/post.js');
@@ -10,7 +11,8 @@ const Post = require('../models/post.js');
 router.post('/post', authenticate, async (req, res) => {
     const username = req.body.user.username;
     const post = req.body.post;
-    if (!post.content) {
+    console.log();
+    if (!post.content || (!_.toLower(post.content).endsWith('.jpg') && !_.toLower(post.content).endsWith('.png') && !_.toLower(post.content).endsWith('.jpg'))) {
         res.status(400).json({ message: 'Post content is required to be a valid image file name' });
     } else {
         try{
